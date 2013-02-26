b828a9a048f0a840fe23a899bf9be89a2b7684e5
Marcin <frankiewicz.marcin@gmail.com>
2013-02-26 16:57:20 +0100

    * Model dla QR

---
+++ src/main/java/pl/fraanek/weddinginvitationsqr/model/QRModel.java
@@ -0,0 +1,13 @@
+package pl.fraanek.weddinginvitationsqr.model;
+
+import java.util.List;
+
+/**
+ * @author Marcin Frankiewicz
+ *         Date: 26.02.13
+ *         Time: 15:29
+ */
+public class QRModel {
+    private int size;
+    private List<String> urls;
+}
